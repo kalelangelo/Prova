413f069a25a9c4b82c6c755e04a4e2252f28710d
kalelangelo <134523626+kalelangelo@users.noreply.github.com>
2023-11-10 16:20:16 -0300

    caramba

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "ENCARE O KIKO!";
 }
+function exibirAlerta() {
 
+    alert("Olá! Isso é um alerta em JavaScript.");
+    
+    }
             
            
