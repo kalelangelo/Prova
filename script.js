a7394044be96a58e570438f8d1fc4bdf388fef0a
kalelangelo <134523626+kalelangelo@users.noreply.github.com>
2023-11-10 15:55:17 -0300

    apaguei

--- script.js
+++ script.js
@@ -2,7 +2,4 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "ENCARE O KIKO!";
 }
-function alert() {
-    document.getElementById("VEM pra cima").innerHTML = "Derrote_O_KIKO";
 
-}
