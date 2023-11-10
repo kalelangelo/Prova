4038bdbab890cd4a9ad6baaa10c4cf08c1778833
kalelangelo <134523626+kalelangelo@users.noreply.github.com>
2023-11-10 15:32:45 -0300

    teclado

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "ENCARE O KIKO!";
 }
+function alert() {
+    document.getElementById(Teclado).innerHTML = Derrote_O_KIKO
+
+}
