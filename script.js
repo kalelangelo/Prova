8b284605574dc0360e5876d2a4e7e2d8584dc350
kalelangelo <134523626+kalelangelo@users.noreply.github.com>
2023-11-10 16:09:43 -0300

    vai

--- script.js
+++ script.js
@@ -2,14 +2,11 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "ENCARE O KIKO!";
 }
-<body>
- <script>
+
 
 function exibirAlerta() {
             
-alert("Olá! Isso é um alerta em JavaScript.")
+alert("Olá! Isso é um alerta em JavaScript.");
+            }
             
-}
-            
-            </script>    
-</body> 
+           
