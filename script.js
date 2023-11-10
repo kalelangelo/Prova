7110606bd41387c1f3f7826c6fec3f98cdb0dc13
kalelangelo <134523626+kalelangelo@users.noreply.github.com>
2023-11-10 15:39:14 -0300

    vai

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function mudarTexto() {
     document.getElementById("demo").innerHTML = "ENCARE O KIKO!";
 }
 function alert() {
-    document.getElementById(Teclado).innerHTML = Derrote_O_KIKO
+    document.getElementById("Teclado").innerHTML = "Derrote_O_KIKO";
 
 }
