f428341103853f75ec36fa623da6d8a8179b933f
kalelangelo <134523626+kalelangelo@users.noreply.github.com>
2023-11-10 15:58:04 -0300

    bootom

--- script.js
+++ script.js
@@ -2,4 +2,14 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "ENCARE O KIKO!";
 }
+<body>
+         <script>
 
+            function exibirAlerta() {
+            
+            alert("Olá! Isso é um alerta em JavaScript.");
+            
+            }
+            
+            </script>    
+</body> 
