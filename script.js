7bf377ed94124c82e9aa2ef43bbc92ba9b0ee532
kalelangelo <134523626+kalelangelo@users.noreply.github.com>
2023-11-10 15:48:43 -0300

    pra cima

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function mudarTexto() {
     document.getElementById("demo").innerHTML = "ENCARE O KIKO!";
 }
 function alert() {
-    document.getElementById("Teclado").innerHTML = "Derrote_O_KIKO";
+    document.getElementById("VEM pra cima").innerHTML = "Derrote_O_KIKO";
 
 }
